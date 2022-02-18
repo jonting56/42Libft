6f07a09bda3a850bd7a20cd99b841c0d5dfb9e67
jonting56 <87597182+jonting56@users.noreply.github.com>
2022-02-18 17:48:47 +1030

    Add files via upload
    Slightly better

--- ft_split.c
+++ ft_split.c
@@ -6,56 +6,66 @@
 /*   By: jting <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 13:45:58 by jting             #+#    #+#             */
-/*   Updated: 2022/02/18 15:57:32 by jting            ###   ########.fr       */
+/*   Updated: 2022/02/18 17:47:43 by jting            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_wordcount(char *s, char c)
+static unsigned int	ft_wordcount(char *s, char c)
 {
-	int	i;
-	int	words;
+	unsigned int	i;
+	unsigned int	words;
 
 	i = 0;
 	words = 0;
 	while (s[i])
 	{
-		if (s[i + 1] == c || s[i + 1] == '\0')
-		   words++;
-		i++;
+		while (s[i] == c)
+			i++;
+		if (s[i] != c && s[i] != '\0')
+			words++;
+		while (s[i] != c && s[i] != '\0')
+			i++;
 	}
 	return (words);
 }	
 
-static	int	ft_len(char const *s, c)
+static	unsigned int	ft_len(char const *s, char c)
 {
-	int	i;
-	int	len;
+	size_t			i;
+	unsigned int	len;
 
 	i = 0;
-	while (s[i] == c)
+	len = 0;
+	while (s[i] == c && s[i])
 		i++;
 	while (s[i] != c && s[i])
+	{	
 		i++;
 		len++;
+	}
 	return (len);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	char	**dst;
-	int		i;
-	int		end;
+	char				**dst;
+	unsigned int			i;
+	unsigned int		start;
 
 	dst = malloc(sizeof(*s) * (ft_wordcount((char *)s, c) + 1));
 	if (!dst)
 		return (dst);
 	i = 0;
+	start = 0;
 	while (i < ft_wordcount((char *)s, c))
 	{
-			dst[i] = malloc(sizeof(char) * (ft_len(s, c) + 1));
-			if (!dst[i])
-				return (NULL);
-			end = ft_chars(s, c);
-
+		while (s[start] == c)
+			start++;
+		dst[i] = ft_substr(s, start, ft_len(s, c));
+		start += ft_len(s, c);
+		i++;
+	}	
+	return (dst);
+}
